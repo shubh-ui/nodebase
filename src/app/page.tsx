465264456dab85196e250796398e96f16e9bee85
shubham <shubhpatil8510@gmail.com>
2025-11-07 01:08:31 +0530

    trpc setup added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 import { Button } from "@/components/ui/button";
+import prisma from "@/lib/db";
+import { caller } from "@/trpc/server";
 
-const Page = () => {
+const Page = async () => {
+  const user = await caller.getUsers()
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center">
+      <span>{JSON.stringify(user)}</span>
       <Button>Click me</Button>
     </div>
   )
