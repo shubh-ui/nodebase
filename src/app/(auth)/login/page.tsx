431bf4658e8b0d4988de9bfb7195e3a21ad6788b
shubham <shubhpatil8510@gmail.com>
2025-11-14 22:51:02 +0530

    added a auth layout

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,12 +1,12 @@
 import { LoginForm } from "@/features/auth/components/login-form"
 import { requireUnAuth } from "@/lib/auth-utils";
+import Image from "next/image";
+import Link from "next/link";
 
 const Login = async () => {
-    await requireUnAuth(); 
+    await requireUnAuth();
     return (
-        <div>
-            <LoginForm />
-        </div>
+        <LoginForm />
     )
 }
 
