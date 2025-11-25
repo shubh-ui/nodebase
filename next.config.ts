931a1d1549e2753095ed36598a08ba734b4993e8
shubham <shubhpatil8510@gmail.com>
2025-11-26 00:31:53 +0530

    Added a base redirect to the workflow route

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [{
+      source:'/',
+      destination:'/workflows',
+      permanent: false
+    }]
+  }
 };
 
 export default nextConfig;
