e14161cfa58f81375f98e83d0ffaf57a5f5ae763
shubham <shubhpatil8510@gmail.com>
2025-11-30 16:30:07 +0530

    Added a rest screen layout with header

---
+++ src/components/app-header.tsx
@@ -0,0 +1,13 @@
+import { SidebarTrigger } from "./ui/sidebar";
+
+const AppHeader = () => {
+    return (
+        <>
+            <header className="flex h-14 shrink-0 items-center gap-2 boder-b px-4 bg-background">
+                <SidebarTrigger />
+            </header>
+        </>
+    )
+}
+
+export default AppHeader;
