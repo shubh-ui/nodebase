3b36c551947b9b0794f8bda89d6083b3b1c9754b
shubham <shubhpatil8510@gmail.com>
2025-11-30 16:18:34 +0530

    Completed sidebar

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,14 +1,15 @@
 "use client"
 
-import {CreditCardIcon, FolderOpenIcon, HistoryIcon, KeyIcon, LockIcon, StarIcon} from "lucide-react";
+import {CreditCardIcon, FolderOpenIcon, HistoryIcon, KeyIcon, LockIcon, LogOutIcon, StarIcon} from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import {usePathname, useRouter} from "next/navigation";
 import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarGroupContent, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "./ui/sidebar";
+import { authClient } from "@/lib/auth-client";
 
 const menuItem = [
     {
-        title: "workflows",
+        title: "Main",
         item: [
             {
                 title: "Workflows",
@@ -31,8 +32,20 @@ const menuItem = [
 ]
 
 const AppSidebar = () => {
+    const pathName = usePathname();
+    const router = useRouter();
     return (
         <Sidebar collapsible="icon">
+            <SidebarHeader>
+                <SidebarMenuItem>
+                    <SidebarMenuButton asChild className="gap-x-4 h-10 px-4">
+                        <Link href="/workflows" prefetch>
+                         <Image src="/logo.svg" width={30} height={30} alt="Nodebase" />
+                         <span className="font-semibold text-sm">Nodebase</span>
+                        </Link>
+                    </SidebarMenuButton>
+                </SidebarMenuItem>
+            </SidebarHeader>
             <SidebarContent>
                 {menuItem.map((group) => (
                     <SidebarGroup key={group.title}>
@@ -41,7 +54,9 @@ const AppSidebar = () => {
                                 <SidebarMenuItem key={item.title}>
                                     <SidebarMenuButton
                                      tooltip={item.title}
-                                     isActive={false}
+                                     isActive={
+                                        item.url == "/" ? pathName  == "/" : pathName.startsWith(item.url)
+                                     }
                                      asChild
                                      className="gap-x-4 h-10 px-4"
                                      >
@@ -56,6 +71,45 @@ const AppSidebar = () => {
                     </SidebarGroup>
                 ))}
             </SidebarContent>
+
+            <SidebarFooter>
+                <SidebarMenu>
+                    <SidebarMenuItem>
+                        <SidebarMenuButton
+                            tooltip="Upgrade to pro"
+                            className="gap-x-4 h-10 px-4"
+                            onClick={() => { }}
+                        >
+                            <StarIcon height={4} width={4} />
+                            <span>Upgrade to Pro</span>
+                        </SidebarMenuButton>
+
+                        <SidebarMenuButton
+                            tooltip="Billing Portal"
+                            className="gap-x-4 h-10 px-4"
+                            onClick={() => { }}
+                        >
+                            <CreditCardIcon height={4} width={4} />
+                            <span>Billing Portal</span>
+                        </SidebarMenuButton>
+
+                        <SidebarMenuButton
+                            tooltip="Sign Out"
+                            className="gap-x-4 h-10 px-4"
+                            onClick={() => authClient.signOut({
+                                fetchOptions: {
+                                    onSuccess: () =>{
+                                        router.push("/");
+                                    }
+                                }
+                            })}
+                        >
+                            <LogOutIcon height={4} width={4} />
+                            <span>Sign out</span>
+                        </SidebarMenuButton>
+                    </SidebarMenuItem>
+                </SidebarMenu>
+            </SidebarFooter>
         </Sidebar>
     )
 }
