648648d3f3c872fc425baa3ee72f4f7277b48978
shubham <shubhpatil8510@gmail.com>
2025-12-11 00:28:28 +0530

    Added a prefetch workflow

--- src/features/workflows/server/prefetch.ts
+++ src/features/workflows/server/prefetch.ts
@@ -4,6 +4,10 @@ import type { inferInput } from "@trpc/tanstack-react-query"
 type Input = inferInput<typeof trpc.workflows.getMany>;
 
 
-export const prefetchWorkflows = (params:Input) => {
+export const prefetchWorkflows = (params: Input) => {
     return prefetch(trpc.workflows.getMany.queryOptions(params));
+}
+
+export const prefetchWorkflow = (id: string) => {
+    return prefetch(trpc.workflows.getOne.queryOptions({ id }));
 }
