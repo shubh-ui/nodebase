8a126c9775d1f3be0d39f930ef8845f16e38195c
shubham <shubhpatil8510@gmail.com>
2025-12-11 22:18:14 +0530

    Updated a procedures create workflow and getone with react flow nodes and edges

--- src/features/workflows/server/routers.ts
+++ src/features/workflows/server/routers.ts
@@ -1,16 +1,25 @@
 import { generateSlug } from "random-word-slugs";
 import prisma from "@/lib/db";
+import type { Node, Edge } from "@xyflow/react";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
 import z from "zod";
 import { PAGINATION } from "@/config/constants";
+import { NodeType } from "@/generated/prisma/enums";
 
 export const workflowRouter = createTRPCRouter({
     create: protectedProcedure.mutation(({ ctx }) => {
         return prisma.workflow.create({
             data: {
                 name: generateSlug(3),
-                userId: ctx.auth.user.id
-            }
+                userId: ctx.auth.user.id,
+                nodes: {
+                    create: {
+                        type: NodeType.INITIAL,
+                        position:{x:0, y:0},
+                        name: NodeType.INITIAL
+                    },
+                },
+            },
         })
     }),
     remove: protectedProcedure
@@ -33,10 +42,36 @@ export const workflowRouter = createTRPCRouter({
         }),
     getOne: protectedProcedure
         .input(z.object({ id: z.string() }))
-        .query(({ ctx, input }) => {
-            return prisma.workflow.findFirstOrThrow({
-                where: { id: input.id, userId: ctx.auth.user.id }
+        .query(async ({ ctx, input }) => {
+            const workflow = await prisma.workflow.findFirstOrThrow({
+                where: { id: input.id, userId: ctx.auth.user.id },
+                include: { nodes: true, connections: true}
             })
+
+            //Transforming the server node to react flow compatible
+            const nodes: Node[] = workflow.nodes.map((node) => ({
+                id: node.id,
+                type: node.type,
+                position : node.position as {x: number, y: number},
+                data : (node.data as Record<string, unknown>) || {}
+            }));
+
+            //Transforming the server connection to react flow compatible
+            const edges: Edge[] = workflow.connections.map((connection) => ({
+                id: connection.id,
+                source: connection.fromNodeId,
+                target: connection.toNodeId,
+                sourceHandle: connection.fromOutput,
+                targetHandle: connection.toInput
+            }));
+
+            return {
+                id: workflow.id,
+                name: workflow.name,
+                nodes,
+                edges
+            }
+
         }),
 
     getMany: protectedProcedure
