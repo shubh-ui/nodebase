15fbc7414e9d0cd1c55fa991c7281c2432671150
shubham <shubhpatil8510@gmail.com>
2025-12-11 20:09:40 +0530

    Installed and added a initial react flow into editor component

--- src/features/editor/components/editor.tsx
+++ src/features/editor/components/editor.tsx
@@ -1,7 +1,19 @@
 "use client";
 
 import { ErrorView, LoadingView } from "@/components/entity-components";
+import { useState, useCallback } from 'react';
+import { ReactFlow, applyNodeChanges, applyEdgeChanges, addEdge, Node, Edge, type NodeChange, type EdgeChange, type Connection } from '@xyflow/react';
+
 import { useSuspenseWorkflow } from "@/features/workflows/hooks/use-workflows";
+
+import '@xyflow/react/dist/style.css';
+
+const initialNodes = [
+    { id: 'n1', position: { x: 0, y: 0 }, data: { label: 'Node 1' } },
+    { id: 'n2', position: { x: 0, y: 100 }, data: { label: 'Node 2' } },
+];
+
+const initialEdges = [{ id: 'n1-n2', source: 'n1', target: 'n2' }];
 
 export const EditorLoading = () => {
     return <LoadingView message="Loading editor..." />
@@ -13,10 +25,33 @@ export const EditorError = () => {
 
 export const Editor = ({ workflowId }: { workflowId: string }) => {
     const { data: workflow } = useSuspenseWorkflow(workflowId);
+    const [nodes, setNodes] = useState<Node[]>(initialNodes);
+    const [edges, setEdges] = useState<Edge[]>(initialEdges);
+
+    const onNodesChange = useCallback(
+        (changes: NodeChange[]) => setNodes((nodesSnapshot) => applyNodeChanges(changes, nodesSnapshot)),
+        [],
+    );
+    const onEdgesChange = useCallback(
+        (changes: EdgeChange[]) => setEdges((edgesSnapshot) => applyEdgeChanges(changes, edgesSnapshot)),
+        [],
+    );
+    const onConnect = useCallback(
+        (params: Connection) => setEdges((edgesSnapshot) => addEdge(params, edgesSnapshot)),
+        [],
+    );
+
     return (
-        <p>
-            {JSON.stringify(workflow, null, 2)}
-        </p>
+        <div className="size-full">
+            <ReactFlow
+                nodes={nodes}
+                edges={edges}
+                onNodesChange={onNodesChange}
+                onEdgesChange={onEdgesChange}
+                onConnect={onConnect}
+                fitView
+            />
+        </div>
     )
 
 };
