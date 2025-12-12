059f73cb2a9f9f115c08a29b4a1fc18fd839673f
shubham <shubhpatil8510@gmail.com>
2025-12-13 00:24:52 +0530

    Created a trigger node and implemanted it with node registry

---
+++ src/features/triggers/components/manual-trigger/node.tsx
@@ -0,0 +1,22 @@
+import { NodeProps } from "@xyflow/react";
+import { memo } from "react";
+import { BaseTriggerNode } from "../base-trigger-node";
+import { MousePointerIcon } from "lucide-react";
+
+
+export const ManualTriggerNode = memo((props: NodeProps) => {
+    return (
+        <>
+         <BaseTriggerNode 
+          {...props}
+          icon={MousePointerIcon}
+          name="When clicking 'Execute Workflow'"
+        //   status={nodeStatus}
+        //   onDoubleClick={handleOpenSetting}
+        //   onSettings={handleOpenSetting}
+         />
+        </>
+    )
+})
+
+ManualTriggerNode.displayName = "ManualTriggerNode"
