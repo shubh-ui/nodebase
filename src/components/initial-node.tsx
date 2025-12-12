b211a226e392c066b86ebd3dcaccc51986205147
shubham <shubhpatil8510@gmail.com>
2025-12-12 19:43:00 +0530

    Added a sheet open onclick on initial node and upper side plus icon

--- src/components/initial-node.tsx
+++ src/components/initial-node.tsx
@@ -1,24 +1,28 @@
 "use client";
 
-import type { NodeProps } from "@xyflow/react"; 
-import { PlusIcon } from "lucide-react"; 
+import type { NodeProps } from "@xyflow/react";
+import { PlusIcon } from "lucide-react";
 import { memo, useState } from "react";
 import { PlaceholderNode } from "./react-flow/placeholder-node";
 import { WorkflowNode } from "@/features/workflows/components/workflow-node";
+import { NodeSelector } from "./node-selector";
 
-export const InitialNode = memo ((props: NodeProps) => {
-return (
-    <WorkflowNode showToolbar={false} >
-        <PlaceholderNode
-            {...props}
-            onClick={() => {}}
-        >
-            <div className="cursor-pointer flex items-center justify-center">
-                <PlusIcon className="size-4" />
-            </div>
-        </PlaceholderNode>
-    </WorkflowNode>
-)
+export const InitialNode = memo((props: NodeProps) => {
+    const [selectorOpen, setSelectorOpen] = useState(false);
+    return (
+        <NodeSelector open={selectorOpen} onOpenChange={setSelectorOpen}>
+            <WorkflowNode showToolbar={false} >
+                <PlaceholderNode
+                    {...props}
+                    onClick={() => { }}
+                >
+                    <div className="cursor-pointer flex items-center justify-center">
+                        <PlusIcon className="size-4" />
+                    </div>
+                </PlaceholderNode>
+            </WorkflowNode>
+        </NodeSelector>
+    )
 })
 
 InitialNode.displayName = "InitialNode";
