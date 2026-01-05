6c0a7e3ea358f905668de3458669cfb00026b9da
shubham <shubhpatil8510@gmail.com>
2026-01-05 23:37:20 +0530

    Added first execute workfow function to log the nodes

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,32 @@
+import prisma from "@/lib/db";
 import { inngest } from "./client";
+import { NonRetriableError } from "inngest";
 
 
 export const executeWorkflow = inngest.createFunction(
   { id: "execute-workflow" },
   { event: "workflow/execute.workflow" },
   async ({ event, step }) => {
+    const workflowId = event.data.workflowId;
     await step.sleep("wait-a-moment-Ai-call", "5s");
+    const nodes = await step.run("prepare workflow", async () => {
+      const workflow = await prisma.workflow.findUnique({
+        where : {
+          id: workflowId
+        },
+        include :{
+          nodes: true,
+          connections: true
+        }
+      })
+
+      if(!workflow) {
+        throw new NonRetriableError("Workflow not found.")
+      }
+
+      return workflow.nodes;
+    })
+
+    return { nodes }
   },
 );
