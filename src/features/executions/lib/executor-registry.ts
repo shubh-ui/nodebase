fd36f2aa238153d0c071727929929e48b1ba0065
shubham <shubhpatil8510@gmail.com>
2026-01-27 00:08:46 +0530

    Added a getExecutor util

--- src/features/executions/lib/executor-registry.ts
+++ src/features/executions/lib/executor-registry.ts
@@ -2,5 +2,16 @@ import { NodeType } from "@/generated/prisma/enums";
 
 export const executorRegistry :Record<NodeType, unknown> = {
 [NodeType.MANUAL_TRIGGER] : () => {},
-[NodeType.HTTP_REQUEST] : () => {}
+[NodeType.HTTP_REQUEST] : () => {},
+[NodeType.INITIAL] : () => {}
+}
+
+
+export const getExecutor = (type: NodeType) : unknown => {
+    const executor = executorRegistry[type];
+    if(!executor) {
+        throw new Error(`No executor registry found for ${type}`)
+    }
+
+    return executor;
 }
